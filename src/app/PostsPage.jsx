47946228cc06c8947c0639410cd847976c6d7f19
gogi981 <goran.zobenica@gmail.com>
2020-01-19 23:14:32 +0100

    all classes converted to functions with hooks

--- src/app/PostsPage.jsx
+++ src/app/PostsPage.jsx
@@ -1,52 +1,44 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import PostsGrid from './components/PostsGrid'
 import SearchBar from './components/SearchBar'
 import searchImage from '../images/search.png'
 import { fetchPosts } from "../services/postServices"
 import Loader from "./components/Loader"
 
-class PostsPage extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            posts: [],
-            loading: true,
-            searchQuery: "",
-        }
-    }
+const PostsPage = () => {
 
-    componentDidMount() {
+    const [posts, setPosts] = useState([])
+    const [loading, setLoading] = useState(true)
+    const [searchQuery, setSearchQuery] = useState("")
+
+    useEffect(() => {
         fetchPosts()
             .then(posts => {
-                this.setState({ posts, loading: false });
+                setPosts(posts)
+                setLoading(false)
             })
+    }, [])
+
+    const getSearchQuery = (searchQuery) => {
+        setSearchQuery(searchQuery)
     }
 
-    getSearchQuery = (searchQuery) => {
-        this.setState({ searchQuery })
-
+    if (loading) {
+        return <Loader />
     }
 
-    render() {
+    const filteredPosts = posts.filter((element) => (element.title + element.subtitle + element.text).toLowerCase().includes(searchQuery))
 
-        if (this.state.loading) {
-            return <Loader />
-        }
-
-        const filteredPosts = this.state.posts.filter((element) => (element.title + element.subtitle + element.text).toLowerCase().includes(this.state.searchQuery))
-
-        return (
-            <div className="postsPage row">
-                <div className="col">
-                    <h2> All Posts</h2>
-                    <img src={searchImage} alt="searchImage" className="searchImage" />
-                    <SearchBar className="searchBar" onChange={this.getSearchQuery} />
-                    <PostsGrid searchQuery={this.state.searchQuery} posts={filteredPosts} />
-                </div>
+    return (
+        <div className="postsPage row">
+            <div className="col">
+                <h2> All Posts</h2>
+                <img src={searchImage} alt="searchImage" className="searchImage" />
+                <SearchBar className="searchBar" onChange={getSearchQuery} />
+                <PostsGrid searchQuery={searchQuery} posts={filteredPosts} />
             </div>
-        )
-    }
-
+        </div>
+    )
 }
 
 export default PostsPage
