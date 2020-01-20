9f89cc13e40a00d386ec0f0e5db06c04b974352a
DZ JJZmaj <goran.zobenica@gmail.com>
2020-01-20 08:17:26 +0100

    added comments

--- src/services/userServices.js
+++ src/services/userServices.js
@@ -1,17 +1,20 @@
 import { http } from './HttpServices'
 
+//to register a new user we are sending user data and user registration endpoint to the post method function in Httpservices.js
 export const createUser = (data) => {
     const requestUrl = "http://crud-api.hypetech.xyz/v1/auth/register"
     return http.post(requestUrl, data)
 }
 
 
+//sending login information and login endpoint to our post method function
 export const loginUser = (data) => {
     const requestUrl = "http://crud-api.hypetech.xyz/v1/auth/login"
     return http.post(requestUrl, data)
 }
 
-
+//getting the user name by ID provided as parameter; 
+//server holds user name in object with property firstName or in property name.first therefore we had to cover both responses with IF statement
 export const searchUser = (id) => {
     const requestUrl = "http://crud-api.hypetech.xyz/v1/users/" + id
     return http.get(requestUrl)
@@ -23,6 +26,7 @@ export const searchUser = (id) => {
         })
 }
 
+//sending endpoint to our get method
 export const fetchUsers = () => {
     const requestUrl = "http://crud-api.hypetech.xyz/v1/users"
     return http.get(requestUrl)
